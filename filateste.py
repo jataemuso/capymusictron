cf3c7cbf103aa40df250375edcc8c1388cc602f6
jataemuso <marcelolapaaa@gmail.com>
2024-12-20 11:47:27 -0300

    criado FILA_TUDO

---
+++ filateste.py
@@ -0,0 +1,11 @@
+FILA_TUDO = [
+    {"title": "Música 1", "added_by": "user1", "downloaded": False},
+    {"title": "Música 2", "added_by": "user1", "downloaded": False},
+    {"title": "Música 3", "added_by": "user2", "downloaded": False},
+    {"title": "Música 4", "added_by": "user2", "downloaded": False},
+    {"title": "Música 5", "added_by": "user3", "downloaded": False}
+]
+
+
+while True:
+    
